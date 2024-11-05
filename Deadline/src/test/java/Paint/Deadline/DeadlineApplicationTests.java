f724032ffd9ccabee67691fe66f8dd180ed11ac8
abdelrahmanmohammednasr <abdoahlawy6161@gmail.com>
2024-11-05 20:46:41 +0200

    created project

---
+++ Deadline/src/test/java/Paint/Deadline/DeadlineApplicationTests.java
@@ -0,0 +1,13 @@
+package Paint.Deadline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
