3ac3c736eb1cfa463cff6d3e4f10995ace18d703
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-11-09 15:41:59 +0200

    vite project created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css';
+// import { useState } from 'react'
+import './App.css'
 
 function App() {
+
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+  
+    </>
+  )
 }
 
-export default App;
+export default App
