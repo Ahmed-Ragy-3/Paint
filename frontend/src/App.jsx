fadaf4e7ed407a5a687f073444feca9860154d5c
Nour Eldien <noureldientaha@gmail.com>
2024-11-12 20:17:35 +0200

    upperbaralmostdoen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-// Import the SVG as an image and rename it to `delete`
-import trash3 from './assets/trash3.svg';
+
+import UpperBar from './UpperBar/UpperBar';
 
 function App() {
   return (
     <>
-      <button>
-        {/* Use the imported SVG as the src for the <img> */}
-        <img src={trash3} alt="Delete icon" width={100} height={100} />
-      </button>
+      <UpperBar></UpperBar>
     </>
   );
 }
--- frontend/src/UpperBar/Align.jsx
+++ frontend/src/UpperBar/Align.jsx
@@ -1,10 +1,17 @@
-// import React from 'react'
-// horizontal align and vertical align
+import './Align.css';
 
 export default function Align() {
   return (
-    <div>
+    <div className="Align" role="group">
       
+      <button className="btn" title='Vertical Align'>
+        <span className="fluent--align-center-vertical-28-filled"></span>
+      </button>
+
+      <button className="btn" title='Horizontal Align'>
+        <span className="fluent--align-center-horizontal-28-filled"></span>
+      </button>
+
     </div>
   )
 }
